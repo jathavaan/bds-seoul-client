9674cc6a163a7ae0750b6e66e33fd4397bd06f08
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-03-24 08:47:47 +0900

    chore: #10 Added author and quote entites and created a one-to-many relationship between them

---
+++ src/domain/entites/author.py
@@ -0,0 +1,23 @@
+﻿from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from ..base import Base
+from .quote import Quote
+
+
+class Author(Base):
+    id: int = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name: str = Column(String(300), nullable=False)
+    about_url: str = Column(String(400), nullable=False)
+
+    quotes: list[Quote] = relationship("Author", back_populates="quotes", cascade="all, delete-orphan")
+
+    def __init__(self, name: str, about_url: str, quotes: list[Quote] = None):
+        super().__init__()
+
+        if quotes is None:
+            quotes = []
+
+        self.name = name
+        self.about_url = about_url
+        self.quotes = quotes
---
+++ src/domain/entites/quote.py
@@ -0,0 +1,16 @@
+﻿from sqlalchemy import Column, Integer, Text
+from sqlalchemy.orm import relationship
+
+from ..base import Base
+from .author import Author
+
+
+class Quote(Base):
+    id: int = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    quote: str = Column(Text, nullable=False)
+
+    author: Author = relationship("Quote", back_populates="author")
+
+    def __init__(self, quote: str) -> None:
+        super().__init__()
+        self.quote = quote
