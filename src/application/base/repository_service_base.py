4b45683543d3786d1b657e549baf5c5651308d70
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-03-24 09:53:02 +0900

    chore: #10 Added DI and other application layer code

---
+++ src/application/base/repository_service_base.py
@@ -0,0 +1,8 @@
+﻿from sqlalchemy.orm import Session
+
+
+class RepositoryServiceBase:
+    session: Session
+
+    def __init__(self, session: Session):
+        self.session = session
---
+++ src/application/container.py
@@ -0,0 +1,9 @@
+﻿from dependency_injector import containers, providers
+
+from ..persistence import create_db_session
+
+
+class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(modules=["src.scraper.pipelines"])
+
+    session = providers.Factory(create_db_session)
