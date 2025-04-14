99710a102e909fa240d8eef7d6ae62ba53b66516
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-04-14 13:01:16 +0900

    chore: #21 Loading enviornment variables from ignored .env file

--- src/config.py
+++ src/config.py
@@ -1,5 +1,10 @@
-﻿from enum import Enum
+﻿import os
+from enum import Enum
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config(Enum):
-    SQLALCHEMY_DATABASE_URI = "mariadb+mariadbconnector://root:seoul-2@127.0.0.1:3306/seoul-2-db"
+    SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://{os.getenv('db_username')}:{os.getenv('db_password')}@{os.getenv('db_host')}:{os.getenv('db_port')}/{os.getenv('db_name')}"
