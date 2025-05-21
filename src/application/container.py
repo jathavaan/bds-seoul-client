adb2eb2e4b33b5081df2039ed68a2432074b67a8
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-05-22 08:56:49 +0900

    chore: #25 Added logger and injected last_scraped_date_producer

--- src/application/container.py
+++ src/application/container.py
@@ -1,5 +1,12 @@
-﻿from dependency_injector import containers
+﻿from dependency_injector import containers, providers
+
+from src import Config
+from src.application.common import Logger
+from src.application.kafka.producers import LastScrapedDateProducer
 
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(modules=["src.scraper.pipelines"])
+
+    logger = providers.Singleton(Logger.get_logger, name="API", level=Config.LOGGING_LEVEL.value)
+    last_scraped_date_producer = providers.Singleton(LastScrapedDateProducer, logger=logger)
--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
-﻿import os
+﻿import logging
+import os
 from enum import Enum
 
 from dotenv import load_dotenv
@@ -11,3 +12,8 @@ class Config(Enum):
     KAFKA_REVIEW_TOPIC = "reviews"
     KAFKA_LAST_SCRAPED_DATE_REQ_TOPIC = "last_scraped_date_requests"
     KAFKA_LAST_SCRAPED_DATE_RES_TOPIC = "last_scraped_date_responses"
+
+    LOGGING_LEVEL = logging.INFO
+    LOGGER_WIDTH_OFFSET = 90
+    SEQ_URL = f"http://{os.getenv('SEQ_SERVER')}:{os.getenv('SEQ_PORT')}"
+    SEQ_LOG_BATCH_SIZE = 1
