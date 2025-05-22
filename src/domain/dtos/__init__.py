2fbcecd423e6dc7f00227ed05daf3836503fcb9d
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-05-22 11:38:16 +0900

    chore: #25 Added review producer

--- src/domain/dtos/__init__.py
+++ src/domain/dtos/__init__.py
@@ -1,1 +1,2 @@
 ﻿from .game import *
+from .review import *
---
+++ src/domain/dtos/review.py
@@ -0,0 +1,10 @@
+﻿from dataclasses import dataclass, field
+
+
+@dataclass
+class ReviewDto:
+    game_id: int = field(metadata={"description": "Steam game ID"})
+    date_posted: str
+    is_recommended: bool
+    hours_played: float
+    user_id: int
