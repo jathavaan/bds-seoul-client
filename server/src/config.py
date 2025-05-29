45f0a7ebd74f87686071983f167a98a1e13e90a2
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-05-29 12:43:53 +0900

    chore: #44 Added config for poll timeout

--- server/src/config.py
+++ server/src/config.py
@@ -16,8 +16,9 @@ class Config(Enum):
     KAFKA_LAST_SCRAPED_DATE_REQ_TOPIC = "last_scraped_date_requests"
     KAFKA_LAST_SCRAPED_DATE_RES_TOPIC = "last_scraped_date_responses"
     KAFKA_POLL_TIMEOUT = 1.0
+    KAFKA_MAX_POLL_TIMEOUT = 86400000
 
-    LOGGING_LEVEL = logging.DEBUG
+    LOGGING_LEVEL = logging.INFO
     LOGGER_WIDTH_OFFSET = 90
     SEQ_URL = f"http://{os.getenv('SEQ_SERVER')}:{os.getenv('SEQ_PORT')}"
     SEQ_LOG_BATCH_SIZE = 1
