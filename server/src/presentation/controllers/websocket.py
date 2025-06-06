896d627cdf30369d659a9d6120d5ea2ff463cb56
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-06-06 14:24:33 +0900

    chore: #59 Established connection between BE and FE with websockets

--- server/src/presentation/controllers/websocket.py
+++ server/src/presentation/controllers/websocket.py
@@ -1,4 +1,5 @@
-﻿from fastapi import APIRouter, WebSocket
+﻿import asyncio
+from fastapi import APIRouter, WebSocket
 from starlette.websockets import WebSocketDisconnect
 
 websocket_router = APIRouter()
@@ -10,8 +11,23 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     connected_clients.append(websocket)
 
+    async def send_periodic():
+        try:
+            while True:
+                await asyncio.sleep(1)
+                await websocket.send_text("Hello from server!")
+        except (WebSocketDisconnect, asyncio.CancelledError):
+            pass
+
+    send_task = asyncio.create_task(send_periodic())
+
     try:
         while True:
-            await websocket.receive_text()
+            await websocket.receive_text()  # Keep the connection alive
     except WebSocketDisconnect:
         connected_clients.remove(websocket)
+        send_task.cancel()
+        try:
+            await send_task
+        except asyncio.CancelledError:
+            pass
