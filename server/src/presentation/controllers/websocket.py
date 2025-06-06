1e3a67ea8aade0ebf0ae2dff57b52ab242fa8904
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-06-06 13:43:38 +0900

    chore: #59 Added websocket router controller

---
+++ server/src/presentation/controllers/websocket.py
@@ -0,0 +1,17 @@
+﻿from fastapi import APIRouter, WebSocket
+from starlette.websockets import WebSocketDisconnect
+
+websocket_router = APIRouter()
+connected_clients: list[WebSocket] = []
+
+
+@websocket_router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    connected_clients.append(websocket)
+
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        connected_clients.remove(websocket)
